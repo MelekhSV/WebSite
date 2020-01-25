c3929d9ae4ffa166fcb3f4fede90d872a196fad6
Sergey Melekh <melex.1998@mail.ru>
2020-01-25 12:27:39 +0300

    create base.html and app Pizza

--- app/Pizza/pizza1/views.py
+++ app/Pizza/pizza1/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+
+def Project_Function(request):
+    return render(request, 'Django/index.html')
+
+def Result_Function(request):
+    return render(request, 'Django/result.html')
