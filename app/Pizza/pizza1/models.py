1bea4db63d778901eee1e9c3a146bf74f4616eb4
Sergey Melekh <melex.1998@mail.ru>
2020-01-26 16:25:00 +0300

    Update and Delete Form

--- app/Pizza/pizza1/models.py
+++ app/Pizza/pizza1/models.py
@@ -4,6 +4,12 @@ from  django.utils.text import slugify
 # Create your models here.
 from time import time
 
+from django.utils.text import slugify
+
+
+def gen_slug(s):
+    new_slug = slugify(s, allow_unicode=True)
+    return new_slug + '-' + str(int(time()))
 
 
 
@@ -13,7 +19,7 @@ class Post(models.Model):
     title = models.CharField(max_length=150, db_index=True)
     slug = models.SlugField(max_length=150,blank=True, unique=True)
     body = models.TextField(blank=True, db_index=True)
-    image = models.ImageField(default=None)
+    image = models.ImageField(blank=True,default=None)
     tags = models.ManyToManyField('Tag',blank=True, related_name='pizzas')
     data_pub = models.DateTimeField(auto_now_add=True)
 
@@ -21,9 +27,20 @@ class Post(models.Model):
     def get_absolute_url(self):
         return reverse('pizza_detail_url', kwargs={'slug': self.slug})
 
+    def get_update_url(self):
+        return reverse('pizza_update_url', kwargs={'slug': self.slug})
+
+
     def __str__(self):
         return self.title
 
+    #
+    # генерация слага по title
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = gen_slug(self.title)
+        super().save(*args, **kwargs)
 
 
 
@@ -35,6 +52,8 @@ class Tag(models.Model):
         return reverse('tag_detail_url', kwargs={'slug':self.slug})
 
 
+    def get_update_url(self):
+        return reverse('tag_update_url', kwargs={'slug': self.slug})
 
     def __str__(self):
         return '{}'.format(self.title)
--- app/Pizza/pizza1/views.py
+++ app/Pizza/pizza1/views.py
@@ -4,6 +4,85 @@ from django.views.generic import View
 from .models import Post, Tag
 from django.shortcuts import get_object_or_404
 from .utils import ObjectDetailMixin
+from .forms import TagForm, PostForm
+
+from django.shortcuts import redirect
+
+from django.urls import reverse
+
+class ObjectCreateMixin:
+    model_form = None
+    template = None
+
+
+    def get(self, request):
+        form = self.model_form()
+        return render(request, self.template, context={'form': form})
+
+    def post(self, request):
+        bound_form = self.model_form(request.POST)
+        if bound_form.is_valid():
+            new_obj = bound_form.save()
+            return redirect(new_obj)
+        return render(request, self.template, context={'form': bound_form})
+
+
+
+
+
+
+
+
+
+
+
+
+class ObjectUpdateMixin:
+    model = None
+    model_form = None
+    template = None
+
+    def get(self, request, slug):
+        obj = self.model.objects.get(slug__iexact=slug)
+        bound_form = self.model_form(instance=obj)
+        return render(request, self.template, context={'form':bound_form, self.model.__name__.lower():obj})
+
+
+    def post(self, request, slug):
+        obj = self.model.objects.get(slug__iexact=slug)
+        bound_form = self.model_form(request.POST, instance=obj)
+
+        if bound_form.is_valid():
+             new_obj = bound_form.save()
+             return redirect(new_obj)
+        return render(request, self.template, context={'form':bound_form, self.model.__name__.lower():obj})
+
+
+
+
+
+class ObjectDeleteMixin:
+    model = None
+    template = None
+    redirect_url = None
+    def get(self, request, slug):
+        obj = self.model.objects.get(slug__iexact = slug)
+        return render(request, self.template, context={self.model.__name__:obj})
+
+    def post(self, request, slug):
+        odj = self.model.objects.get(slug__iexact=slug)
+        odj.delete()
+        return render(reverse(self.redirect_url))
+
+
+
+
+
+
+
+
+
+
 
 
 def pizza_list(request):
@@ -31,6 +110,34 @@ class PostDetail(ObjectDetailMixin,View):
 #     pizza = Post.objects.get(slug__iexact= slug)
 #     return render(request, 'Pizza/pizza_detail.html', context={'pizza':pizza})
 
+
+
+class PostCreate(ObjectCreateMixin, View):
+    model_form = PostForm
+    template = 'Pizza/pizza_create_form.html'
+
+    # def get(self,request):
+    #     form = PostForm()
+    #     return render(request, 'Pizza/pizza_create_form.html', context={'form':form})
+    #
+    # def post (self,request):
+    #     bound_from = PostForm(request.POST)
+    #     if bound_from.is_valid():
+    #         new_post = bound_from.save()
+    #         return redirect(new_post)
+    #     return render(request, 'Pizza/pizza_create_form.html', context={'form': bound_from})
+
+
+class PostUpdate(ObjectUpdateMixin, View):
+    model = Post
+    model_form = PostForm
+    template = 'Pizza/pizza_update_form.html'
+
+
+
+
+
+
 def tags_list(request):
     tags = Tag.objects.all()
     return render(request, 'Pizza/tags_list.html', context = {'tags': tags})
@@ -57,3 +164,74 @@ class TagDetail(ObjectDetailMixin,View):
 #
 #     # tags = Tag.objects.all()
 #     return render(request, 'Pizza/tag_detail.html', context={'tag':tag})
+
+class TagCreate(ObjectCreateMixin,View):
+    model_form = TagForm
+    template = 'Pizza/tag_create.html'
+
+
+
+     # def get(self,request):
+     #     form = TagForm()
+     #     return render(request, 'Pizza/tag_create.html', context={'form':form})
+     #
+     # def post(self,request):
+     #
+     #     bound_form = TagForm(request.POST)
+     #     if bound_form.is_valid():
+     #         new_tag = bound_form.save()
+     #         return redirect(new_tag)
+     #     return render(request, 'Pizza/tags_list.html', context={'form':bound_form} )
+
+class TagUpdate(ObjectUpdateMixin, View):
+    model = Tag
+    model_form = TagForm
+    template = 'Pizza/tag_update_form.html'
+
+    # def get(self, request, slug):
+    #     tag = Tag.objects.get(slug__iexact=slug)
+    #     bound_form = TagForm(instance=tag)
+    #     return render(request, 'Pizza/tag_update_form.html', context={'form':bound_form, 'tag':tag})
+    #
+    #
+    # def post(self, request, slug):
+    #     tag = Tag.objects.get(slug__iexact=slug)
+    #     bound_form = TagForm(request.POST, instance=tag)
+    #
+    #     if bound_form.is_valid():
+    #          new_tag = bound_form.save()
+    #          return redirect(new_tag)
+
+    #     return render(request, 'Pizza/tag_update_form', context={'form':bound_form, 'tag':tag})
+
+
+class TagDelete(View):
+
+    # model = Tag
+    # template = 'Pizza/tag_delete_form.html'
+    # redirect_url = 'tags_list_url'
+
+    def get(self, request, slug):
+        tag = Tag.objects.get(slug__iexact = slug)
+        return render(request, 'Pizza/tag_delete_form.html', context={'tag':tag})
+
+    def post(self, request, slug):
+        tag = Tag.objects.get(slug__iexact=slug)
+        tag.delete()
+        return render(reverse('tags_list_url'))
+
+
+class PostDelete(View):
+
+    # model = Tag
+    # template = 'Pizza/tag_delete_form.html'
+    # redirect_url = 'tags_list_url'
+
+    def get(self, request, slug):
+        post = Post.objects.get(slug__iexact = slug)
+        return render(request, 'Pizza/pizza_delete_form.html', context={'post':post})
+
+    def post(self, request, slug):
+        pizz = Post.objects.get(slug__iexact=slug)
+        pizz.delete()
+        return render(reverse('pizza_list_url'))
